c6cc99e3ccb29bd2579fd4364d694bcdd3832d07
nyvek28 <kevynquiros@gmail.com>
2015-11-23 08:14:30 -0600

    Creacion MultiUsuario
    Se creo el Multi de la clase usuario

---
+++ src/MultiUsuario.java
@@ -0,0 +1,25 @@
+import java.sql.ResultSet;
+
+class MultiUsuario {
+
+	public Usuario crear(String pnombre, String papellido, int pid, String pemail, String pdireccion, int ptelefono){
+		return null;
+	}
+	
+	public Usuario buscar(int id){
+		return null;
+	}
+	
+	public ResultSet buscar(String nombre){
+		return null;
+	}
+	
+	public void actualizar(Usuario u){
+		
+	}
+	
+	public void borrar(Usuario u, String pnombre, String papellido, int pid, String pemail, String pdireccion, int ptelefono){
+		
+	}
+	
+}
